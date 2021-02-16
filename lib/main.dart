58db8f9665cc877c39147af706f8606d04c058ef
rivaldofez <rivaldofez@gmail.com>
2021-02-16 23:44:38 +0700

    using flexible layout

--- lib/main.dart
+++ lib/main.dart
@@ -3,39 +3,59 @@ import 'dart:math';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Random random = Random();
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Latihan Animated Container"),
-        ),
-        body: Center(
-          child: GestureDetector(
-            onTap: (){
-              setState(() {
-
-              });
-            },
-            child: AnimatedContainer(
-              color: Color.fromARGB(255, random.nextInt(256), random.nextInt(256), random.nextInt(256)),
-              duration: Duration(seconds: 1),
-              width: 50.0 + random.nextInt(101),
-              height: 50.0 + random.nextInt(101),
+        appBar: AppBar(title: Text("Flexible Layout")),
+        body: Column(
+          children: [
+            Flexible(
+              flex: 1,
+              child: Row(
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      margin: EdgeInsets.all(5),
+                      color: Colors.indigo,
+                    ),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      margin: EdgeInsets.all(5),
+                      color: Colors.lightGreen,
+                    ),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      margin: EdgeInsets.all(5),
+                      color: Colors.tealAccent,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
+            Flexible(
+              flex: 2,
+              child: Container(
+                margin: EdgeInsets.all(5),
+                color: Colors.green,
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              child: Container(
+                margin: EdgeInsets.all(5),
+                color: Colors.amber,
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
-

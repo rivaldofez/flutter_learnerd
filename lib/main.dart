a003f2667c38282c50cea4d71fb19726e875c65f
rivaldofez <rivaldofez@gmail.com>
2021-02-16 23:08:10 +0700

    using list and listview

--- lib/main.dart
+++ lib/main.dart
@@ -2,29 +2,55 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Widget> widgets = [];
+  int counter = 1;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Latihan TextStyle"),
+          title: Text("Latihan ListView",),
         ),
-        body: Center(
-          child: Text(
-            "Ini adalah Text",
-            style: TextStyle(
-                fontFamily: "Kanit",
-                fontSize: 30,
-                fontStyle: FontStyle.italic,
-                decoration: TextDecoration.overline,
-                decorationColor: Colors.red,
-                decorationThickness: 5,
-                decorationStyle: TextDecorationStyle.wavy,
+        body: ListView(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                RaisedButton(
+                  child: Text("Tambah Data"),
+                  onPressed: (){
+                    setState(() {
+                      widgets.add(Text("Data ke-"+counter.toString(), style: TextStyle(fontSize: 35),));
+                      counter ++;
+                    });
+                  },
+                ),
+                RaisedButton(
+                  child: Text("Hapus Data"),
+                  onPressed: (){
+                    setState(() {
+                      widgets.removeLast();
+                      counter--;
+                    });
+                  },
+                ),
+              ],
             ),
-          ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: widgets,
+            ),
+          ],
         ),
       ),
     );
   }
 }
+

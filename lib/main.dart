4f29491ca77c237d7c48ba697a8be562a1facb1a
rivaldofez <rivaldofez@gmail.com>
2021-02-16 23:24:42 +0700

    using animated container and gesture detector

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(MyApp());
 
@@ -8,49 +9,33 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  List<Widget> widgets = [];
-  int counter = 1;
+  Random random = Random();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Latihan ListView",),
+          title: Text("Latihan Animated Container"),
         ),
-        body: ListView(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                RaisedButton(
-                  child: Text("Tambah Data"),
-                  onPressed: (){
-                    setState(() {
-                      widgets.add(Text("Data ke-"+counter.toString(), style: TextStyle(fontSize: 35),));
-                      counter ++;
-                    });
-                  },
-                ),
-                RaisedButton(
-                  child: Text("Hapus Data"),
-                  onPressed: (){
-                    setState(() {
-                      widgets.removeLast();
-                      counter--;
-                    });
-                  },
-                ),
-              ],
+        body: Center(
+          child: GestureDetector(
+            onTap: (){
+              setState(() {
+
+              });
+            },
+            child: AnimatedContainer(
+              color: Color.fromARGB(255, random.nextInt(256), random.nextInt(256), random.nextInt(256)),
+              duration: Duration(seconds: 1),
+              width: 50.0 + random.nextInt(101),
+              height: 50.0 + random.nextInt(101),
             ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: widgets,
-            ),
-          ],
+          ),
         ),
       ),
     );
   }
 }
+
 

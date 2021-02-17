1c0f4941c56a69d22bc8afc08383363195242e88
rivaldofez <rivaldofez@gmail.com>
2021-02-17 13:06:09 +0700

    using gradient and pattern appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_learnerd/login_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,7 +6,38 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginPage(),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Icon(
+            Icons.adb,
+            color: Colors.white,
+          ),
+          title: Text(
+            "AppBar Example",
+            style: TextStyle(color: Colors.white),
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: (){},
+            ),
+            IconButton(
+              icon: Icon(Icons.apps),
+              onPressed: (){},
+            ),
+          ],
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Color(0xff0096ff), Color(0xff6610f2)],
+                begin: FractionalOffset.topLeft,
+                end: FractionalOffset.bottomRight,
+              ),
+              image: DecorationImage(image: AssetImage("images/pattern.png"), fit: BoxFit.contain, repeat: ImageRepeat.repeat)
+            ),
+          ),
+        ),
+      ),
     );
   }
 }

dca718d403cd7ce601c72e938295be536a6f87e3
rivaldofez <rivaldofez@gmail.com>
2021-02-17 22:38:08 +0700

    using textfield decoration

--- lib/main.dart
+++ lib/main.dart
@@ -23,8 +23,21 @@ class _MyAppState extends State<MyApp> {
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               TextField(
+                decoration: InputDecoration(
+                  fillColor: Colors.lightBlue[50],
+                  filled: true,
+                  icon: Icon(Icons.email),
+                  suffix: Container(width: 5, height: 5, color: Colors.red,),
+                  prefixIcon: Icon(Icons.email),
+                  prefixText: "Name :",
+                  prefixStyle: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+                  labelText: "Nama Lengkap :",
+                  labelStyle: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                  hintText: "Nama Lengkapnya lho.",
+                  hintStyle: TextStyle(fontSize: 12),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(20))
+                ),
                 maxLength: 5,
-                obscureText: true,
                 onChanged: (value){
                   setState(() {
 

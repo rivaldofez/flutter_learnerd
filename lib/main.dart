dc81769fd7143dc283b80a24a38752752cf73a59
rivaldofez <rivaldofez@gmail.com>
2021-02-18 13:57:56 +0700

    using custom card widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,54 +6,120 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Latihan Membuat Button"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              RaisedButton(
-                color: Colors.amber,
-                child: Text("Raised Button"),
-                shape: StadiumBorder(),
-                onPressed: (){},
-              ),
-              Material(
-                borderRadius: BorderRadius.circular(20),
-                elevation: 2,
-                child: Container(
-                  width: 150,
-                  height: 40,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    gradient: LinearGradient(
-                      colors: [
-                        Colors.purple,
-                        Colors.pink
-                      ],
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                    )
-                  ),
-                  child: Material(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.transparent,
-                    child: InkWell(
-                      onTap: (){},
-                      splashColor: Colors.amber,
-                      borderRadius: BorderRadius.circular(20),
-                      child: Center(child: Text("My Button", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),)),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      home: MainPage(),
     );
   }
 }
 
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Custom Card Example",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Color(0xFF8C062F),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xFFFE5788),
+                  Color(0xFFF56D5D),
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+          ),
+          Center(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: MediaQuery.of(context).size.height * 0.7,
+              child: Card(
+                elevation: 10,
+                child: Stack(
+                  children: [
+                    Opacity(
+                      opacity: 0.7,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(4),
+                          image: DecorationImage(
+                            image: AssetImage("images/pattern.png"),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.35,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(4),
+                            topRight: Radius.circular(4)),
+                        image: DecorationImage(
+                            image: AssetImage("images/pemandangan.jpg"),
+                            fit: BoxFit.cover),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(
+                          20,
+                          50 + MediaQuery.of(context).size.height * 0.35,
+                          20,
+                          20),
+                      child: Center(
+                        child: Column(
+                          children: [
+                            Text("Beautifull Sakura at Japan",
+                                style: TextStyle(
+                                    color: Color(0xFFF56D5D), fontSize: 35),
+                                maxLines: 2,
+                                textAlign: TextAlign.center),
+                            Container(
+                              margin: EdgeInsets.fromLTRB(0, 20, 0, 15),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text("Posted on",
+                                      style: TextStyle(
+                                          color: Colors.grey, fontSize: 12),),
+                                  Text("June 18, 2021",
+                                      style: TextStyle(
+                                          color: Color(0xFFF56D5D), fontSize: 12),),
+                                ],
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                Spacer(flex: 10,),
+                                Icon(Icons.thumb_up, size: 18,color: Colors.grey,),
+                                Spacer(flex: 1,),
+                                Text("99", style: TextStyle(color: Colors.grey),),
+                                Spacer(flex: 5,),
+                                Icon(Icons.comment, size: 18,color: Colors.grey,),
+                                Spacer(flex: 1),
+                                Text("88", style: TextStyle(color: Colors.grey),),
+                                Spacer(flex: 10,),
+
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

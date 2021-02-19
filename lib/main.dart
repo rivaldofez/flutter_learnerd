08c696eb96a6126db0d7363ce39c878c113a1644
rivaldofez <rivaldofez@gmail.com>
2021-02-19 11:46:43 +0700

    create decoration TabBar

--- lib/main.dart
+++ lib/main.dart
@@ -5,50 +5,43 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    TabBar myTabBar = TabBar(
+      indicator: BoxDecoration(
+          color: Colors.red,
+          border: Border(top: BorderSide(color: Colors.purple, width: 5),),),
+    tabs: [
+    Tab(
+    icon: Icon(Icons.comment),
+    text: "Comments",
+    ),
+    Tab(
+    icon: Icon(Icons.computer),
+    text: "Computer",
+    ),
+    ],
+    );
+
     return MaterialApp(
-      home: DefaultTabController(
-        length: 4,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text("Contoh Tab Bar"),
-            bottom: TabBar(
-              tabs: [
-                Tab(
-                  icon: Icon(Icons.comment),
-                  text: "Comments",
-                ),
-                Tab(
-                  child: Image(
-                    image: AssetImage("images/seon_hoo.jpg"),
-                  ),
-                ),
-                Tab(
-                  icon: Icon(Icons.computer),
-                ),
-                Tab(
-                  text: "News",
-                ),
-              ],
-            ),
-          ),
-          body: TabBarView(
-            children: [
-              Center(
-                child: Text("Tab 1"),
-              ),
-              Center(
-                child: Text("Tab 2"),
-              ),
-              Center(
-                child: Text("Tab 3"),
-              ),
-              Center(
-                child: Text("Tab 4"),
-              ),
-            ],
-          ),
-        ),
-      ),
+    home: DefaultTabController(
+    length: 2,
+    child: Scaffold(
+    appBar: AppBar(
+    title: Text("Contoh Tab Bar"),
+    bottom: PreferredSize(
+    preferredSize: Size.fromHeight(myTabBar.preferredSize.height),
+    child: Container(color: Colors.amber, child: myTabBar))),
+    body: TabBarView(
+    children: [
+    Center(
+    child: Text("Tab 1"),
+    ),
+    Center(
+    child: Text("Tab 2"),
+    ),
+    ],
+    ),
+    ),
+    ),
     );
   }
 }

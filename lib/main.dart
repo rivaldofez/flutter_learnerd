d5de28df3127f518fe7461b68e74372e853ec201
rivaldofez <rivaldofez@gmail.com>
2021-02-19 11:17:58 +0700

    using hero animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,213 +1,84 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-void main() {
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
-      .then((_) {
-    runApp(MyApp());
-  });
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: MainPage(),
     );
   }
 }
 
 class MainPage extends StatelessWidget {
-  double getSmallDiameter(BuildContext context) =>
-      MediaQuery.of(context).size.width * 2 / 3;
-
-  double getBigDiameter(BuildContext context) =>
-      MediaQuery.of(context).size.width * 7 / 8;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFEEEEEE),
-      body: Stack(
-        children: [
-          Positioned(
-            right: -getSmallDiameter(context) / 3,
-            top: -getSmallDiameter(context) / 3,
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          "Latihan Hero Animation",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: GestureDetector(
+        onTap: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return SecondPage();
+          }));
+        },
+        child: Hero(
+          tag: 'pp',
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(50),
             child: Container(
-              width: getSmallDiameter(context),
-              height: getSmallDiameter(context),
-              decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  gradient: LinearGradient(
-                      colors: [Color(0xFFB226B2), Color(0xFFFF6DA7)],
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter)),
-            ),
-          ),
-          Positioned(
-            left: -getBigDiameter(context) / 4,
-            top: -getBigDiameter(context) / 4,
-            child: Container(
-              child: Center(
-                child: Text(
-                  "Kunairoo",
-                  style: TextStyle(
-                      fontFamily: "KanitRegular",
-                      color: Colors.white,
-                      fontSize: 30),
-                ),
+              width: 100,
+              height: 100,
+              child: Image(
+                fit: BoxFit.cover,
+                image: NetworkImage(
+                    "https://mottokorea.co.kr/wp-content/uploads/2020/11/image_readtop_2020_1230909_16067202344452313.jpg"),
               ),
-              width: getBigDiameter(context),
-              height: getBigDiameter(context),
-              decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  gradient: LinearGradient(
-                      colors: [Color(0xFFB226B2), Color(0xFFFF6DA7)],
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter)),
             ),
           ),
-          Positioned(
-            right: -getBigDiameter(context) / 2,
-            bottom: -getBigDiameter(context) / 2,
-            child: Container(
-              width: getBigDiameter(context),
-              height: getBigDiameter(context),
-              decoration: BoxDecoration(
-                  shape: BoxShape.circle, color: Color(0xFFF3E9EE)),
-            ),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: ListView(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(color: Colors.grey),
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  margin: EdgeInsets.fromLTRB(20, 300, 20, 10),
-                  padding: EdgeInsets.fromLTRB(10, 0, 10, 25),
-                  child: Column(
-                    children: [
-                      TextField(
-                        decoration: InputDecoration(
-                            icon: Icon(
-                              Icons.email,
-                              color: Color(0xFFFF4891),
-                            ),
-                            focusedBorder: UnderlineInputBorder(
-                                borderSide:
-                                    BorderSide(color: Color(0xFFFF4891))),
-                            labelText: "Email:",
-                            labelStyle: TextStyle(color: Color(0xFFFF4891))),
-                      ),
-                      TextField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                            icon: Icon(
-                              Icons.vpn_key,
-                              color: Color(0xFFFF4891),
-                            ),
-                            focusedBorder: UnderlineInputBorder(
-                                borderSide:
-                                    BorderSide(color: Color(0xFFFF4891))),
-                            labelText: "Password:",
-                            labelStyle: TextStyle(color: Color(0xFFFF4891))),
-                      ),
-                    ],
-                  ),
-                ),
-                Align(
-                    alignment: Alignment.centerRight,
-                    child: Container(
-                        margin: EdgeInsets.fromLTRB(0, 0, 20, 20),
-                        child: Text(
-                          "Forgot Password ?",
-                          style:
-                              TextStyle(color: Color(0xFFFF4891), fontSize: 20),
-                        ))),
-                Container(
-                  margin: EdgeInsets.fromLTRB(20, 0, 20, 30),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.5,
-                        height: 40,
-                        child: Container(
-                          child: Material(
-                            borderRadius: BorderRadius.circular(20),
-                            color: Colors.transparent,
-                            child: InkWell(
-                              borderRadius: BorderRadius.circular(20),
-                              splashColor: Colors.amber,
-                              onTap: () {},
-                              child: Center(
-                                child: Text(
-                                  "Sign In",
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.w700),
-                                ),
-                              ),
-                            ),
-                          ),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            gradient: LinearGradient(
-                              colors: [Color(0xFFB226B2), Color(0xFFFF6DA7)],
-                              begin: Alignment.topCenter,
-                              end: Alignment.bottomCenter,
-                            ),
-                          ),
-                        ),
-                      ),
-                      FloatingActionButton(
-                        elevation: 0,
-                        onPressed: () {},
-                        mini: true,
-                        child: Image(
-                          image: AssetImage("images/seon_hoo.jpg"),
-                        ),
-                      ),
-                      FloatingActionButton(
-                        elevation: 0,
-                        onPressed: () {},
-                        mini: true,
-                        child: Image(
-                          image: AssetImage("images/seon_hoo.jpg"),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Dont Have an Account ?  ",
-                      style: TextStyle(
-                          fontSize: 11,
-                          color: Colors.grey,
-                          fontWeight: FontWeight.w500),
-                    ),
-                    Text(
-                      "Sign Up ?",
-                      style: TextStyle(
-                          fontSize: 11,
-                          color: Color(0xFFFF4891),
-                          fontWeight: FontWeight.w700),
-                    )
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
 }
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.pink,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          "Latihan Hero Animation",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Center(
+        child: Hero(
+          tag: 'pp',
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(100),
+            child: Container(
+              width: 200,
+              height: 200,
+              child: Image(
+                fit: BoxFit.cover,
+                image: NetworkImage(
+                    "https://mottokorea.co.kr/wp-content/uploads/2020/11/image_readtop_2020_1230909_16067202344452313.jpg"),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+

78271a55baea98131889b9e88c8e1f0ddaec9f1d
rivaldofez <rivaldofez@gmail.com>
2021-02-19 11:58:11 +0700

    using QRCode Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,28 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:qr/qr.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    TabBar myTabBar = TabBar(
-      indicator: BoxDecoration(
-          color: Colors.red,
-          border: Border(top: BorderSide(color: Colors.purple, width: 5),),),
-    tabs: [
-    Tab(
-    icon: Icon(Icons.comment),
-    text: "Comments",
-    ),
-    Tab(
-    icon: Icon(Icons.computer),
-    text: "Computer",
-    ),
-    ],
-    );
-
     return MaterialApp(
-    home: DefaultTabController(
-    length: 2,
-    child: Scaffold(
-    appBar: AppBar(
-    title: Text("Contoh Tab Bar"),
-    bottom: PreferredSize(
-    preferredSize: Size.fromHeight(myTabBar.preferredSize.height),
-    child: Container(color: Colors.amber, child: myTabBar))),
-    body: TabBarView(
-    children: [
-    Center(
-    child: Text("Tab 1"),
-    ),
-    Center(
-    child: Text("Tab 2"),
-    ),
-    ],
-    ),
-    ),
-    ),
+      home: Scaffold(
+        body: Center(
+          child: QrImage(
+            version: 6,
+            backgroundColor: Colors.grey,
+            foregroundColor: Colors.black,
+            errorCorrectionLevel: QrErrorCorrectLevel.M,
+            padding: EdgeInsets.all(30),
+            size: 300,
+            data: "https://github.com/rivaldofez",
+          ),
+        ),
+      ),
     );
   }
 }
+

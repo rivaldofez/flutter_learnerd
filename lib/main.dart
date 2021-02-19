975db0780e895763d4121a8d3ee1d35dd5485115
rivaldofez <rivaldofez@gmail.com>
2021-02-19 11:29:35 +0700

    using custom size appbar

--- lib/main.dart
+++ lib/main.dart
@@ -6,40 +6,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainPage(),
-    );
-  }
-}
-
-class MainPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue,
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text(
-          "Latihan Hero Animation",
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
-      body: GestureDetector(
-        onTap: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context) {
-            return SecondPage();
-          }));
-        },
-        child: Hero(
-          tag: 'pp',
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(50),
-            child: Container(
-              width: 100,
-              height: 100,
-              child: Image(
-                fit: BoxFit.cover,
-                image: NetworkImage(
-                    "https://mottokorea.co.kr/wp-content/uploads/2020/11/image_readtop_2020_1230909_16067202344452313.jpg"),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(200),
+          child: AppBar(
+            backgroundColor: Colors.amber,
+            flexibleSpace: Container(
+              margin: EdgeInsets.all(20),
+              child: Text(
+                "AppBar with custom height",
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700),
               ),
             ),
           ),
@@ -48,37 +28,3 @@ class MainPage extends StatelessWidget {
     );
   }
 }
-
-class SecondPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.pink,
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text(
-          "Latihan Hero Animation",
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
-      body: Center(
-        child: Hero(
-          tag: 'pp',
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(100),
-            child: Container(
-              width: 200,
-              height: 200,
-              child: Image(
-                fit: BoxFit.cover,
-                image: NetworkImage(
-                    "https://mottokorea.co.kr/wp-content/uploads/2020/11/image_readtop_2020_1230909_16067202344452313.jpg"),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-

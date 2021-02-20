92c659f5376a032090de4a8e0dc9c62cc440f6d3
rivaldofez <rivaldofez@gmail.com>
2021-02-21 01:52:29 +0700

    using animation switch

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_learnerd/user_model.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,30 +9,62 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String output = "no data";
+  bool isOn = false;
+
+  Widget myWidget = Container(
+    width: 200,
+    height: 100,
+    decoration: BoxDecoration(
+        color: Colors.red, border: Border.all(color: Colors.black54)),
+  );
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Api Demo")),
+        appBar: AppBar(
+          title: Text("Animated Switcher"),
+        ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Text(output),
-              RaisedButton(
-                onPressed: () {
-                  User.getUsers("1").then((value){
-                    output="";
-                    for(int i = 0; i < value.length; i++)
-                      output = output + "[ " + value[i].name +" ] ";
-                    setState(() {
-
-                    });
+              AnimatedSwitcher(
+                child: myWidget,
+                duration: Duration(seconds: 1),
+                transitionBuilder: (child, animation)=> RotationTransition(
+                  turns: animation,
+                  child: child,
+                )
+              ),
+              Switch(
+                value: isOn,
+                inactiveTrackColor: Colors.red[200],
+                inactiveThumbColor: Colors.red,
+                activeColor: Colors.green,
+                onChanged: (value) {
+                  isOn = value;
+                  setState(() {
+                    if (isOn)
+                      myWidget = Container(
+                        key: ValueKey(1),
+                        width: 200,
+                        height: 100,
+                        decoration: BoxDecoration(
+                            color: Colors.green,
+                            border: Border.all(color: Colors.black54)),
+                      );
+                    else
+                      myWidget = Container(
+                        key: ValueKey(2),
+                        width: 200,
+                        height: 100,
+                        decoration: BoxDecoration(
+                            color: Colors.red,
+                            border: Border.all(color: Colors.black54)),
+                      );
                   });
                 },
-                child: Text("GET"),
               ),
             ],
           ),

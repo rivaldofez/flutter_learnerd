4e0e117e087bb6544c5fe8d26fffa6bfedf0c30c
rivaldofez <rivaldofez@gmail.com>
2021-02-22 12:19:58 +0700

    using provider state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,90 +1,59 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_learnerd/application_color.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  double myPadding = 5;
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Animated Padding"),
-        ),
-        body: Column(
-          children: [
-            Flexible(
-              flex: 1,
-              child: Row(
-                children: [
-                  Flexible(
-                    flex: 1,
-                    child: AnimatedPadding(
-                      duration: Duration(seconds: 1),
-                      padding: EdgeInsets.all(myPadding),
-                      child: GestureDetector(
-                        onTap: (){
-                          setState(() {
-                            myPadding = 20;
-                          });
+      home: ChangeNotifierProvider<ApplicationColor>(
+        builder: (context) => ApplicationColor(),
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            title: Consumer<ApplicationColor>(
+              builder: (context, applicationColor, _) => Text(
+                "Provicer State Management",
+                style: TextStyle(color: applicationColor.color),
+              ),
+            ),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Consumer<ApplicationColor>(
+                  builder:(context, applicationColor, _) => AnimatedContainer(
+                    margin: EdgeInsets.all(5),
+                    width: 100,
+                    height: 100,
+                    color: applicationColor.color,
+                    duration: Duration(microseconds: 500),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(margin: EdgeInsets.all(5), child: Text("AB")),
+                    Consumer<ApplicationColor>(
+                      builder: (context, applicationColor, _) => Switch(
+                        value: applicationColor.isLightBlue,
+                        onChanged: (newValue) {
+                          applicationColor.isLightBlue = newValue;
                         },
-                        child: Container(
-                          color: Colors.red,
-                        ),
                       ),
                     ),
-                  ),
-                  Flexible(
-                    flex: 1,
-                    child: AnimatedPadding(
-                      duration: Duration(seconds: 1),
-                      padding: EdgeInsets.all(myPadding),
-                      child: Container(
-                        color: Colors.green,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+                  ],
+                )
+              ],
             ),
-            Flexible(
-              flex: 1,
-              child: Row(
-                children: [
-                  Flexible(
-                    flex: 1,
-                    child: AnimatedPadding(
-                      duration: Duration(seconds: 1),
-                      padding: EdgeInsets.all(myPadding),
-                      child: Container(
-                        color: Colors.blue,
-                      ),
-                    ),
-                  ),
-                  Flexible(
-                    flex: 1,
-                    child: AnimatedPadding(
-                      duration: Duration(seconds: 1),
-                      padding: EdgeInsets.all(myPadding),
-                      child: Container(
-                        color: Colors.yellow,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
   }
 }
-

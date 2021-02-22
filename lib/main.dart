6d23f7712f1acb98c316752ab705c9276b5abd99
rivaldofez <rivaldofez@gmail.com>
2021-02-22 21:26:46 +0700

    create product cart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'dart:async';
+import 'package:flutter_learnerd/product_card.dart';
 import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
@@ -9,46 +9,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Custom Progress Bar"),
+          backgroundColor: firstColor,
         ),
-        body: Center(
-          child: ChangeNotifierProvider(
-            builder: (context) => TimeState(),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Consumer<TimeState>(
-                  builder:(context, timestate, _) => CustomProgressBar(
-                    width: 200,
-                    value: timestate.time,
-                    totalValue: 15,
-                  ),
+        body: ChangeNotifierProvider<ProductState>(
+          builder: (context) => ProductState(),
+          child: Container(
+            margin: EdgeInsets.all(20),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Consumer<ProductState>(
+                builder: (context, productState, _) => ProductCard(
+                  imgURL: "images/pemandangan.jpg",
+                  name: "Buah-buahan Mix 1 Kg",
+                  price: "Rp25.000",
+                  onAddCartTap: () {},
+                  quantity: productState.quantity,
+                  onIncTap: () {
+                    productState.quantity += 1;
+                  },
+                  onDecTap: () {
+                    productState.quantity -=1;
+                  },
+                  notification: (productState.quantity > 5) ? "Diskon 10%" : null,
                 ),
-                SizedBox(
-                  height: 10,
-                ),
-                Consumer<TimeState>(
-                  builder: (context, timeState, _) => RaisedButton(
-                    onPressed: () {
-                      Timer.periodic(Duration(seconds: 1), (timer) {
-                        if(timeState.time == 0){
-                          timer.cancel();
-                        }else{
-                          timeState.time -= 1;
-                        }
-                      });
-                    },
-                    child: Text(
-                      "Start",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    color: Colors.lightBlue,
-                  ),
-                ),
-              ],
+              ),
             ),
           ),
         ),
@@ -57,66 +44,13 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class CustomProgressBar extends StatelessWidget {
-  final double width;
-  final int value;
-  final int totalValue;
+class ProductState with ChangeNotifier {
+  int _quantity = 0;
 
-  CustomProgressBar({this.width, this.value, this.totalValue});
+  int get quantity => _quantity;
 
-  @override
-  Widget build(BuildContext context) {
-    double ratio = value / totalValue;
-
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(
-          Icons.timer,
-          color: Colors.grey[700],
-        ),
-        SizedBox(
-          width: 5,
-        ),
-        Stack(
-          children: [
-            Container(
-              width: width,
-              height: 10,
-              decoration: BoxDecoration(
-                  color: Colors.grey[300],
-                  borderRadius: BorderRadius.circular(5)),
-            ),
-            Material(
-              borderRadius: BorderRadius.circular(5),
-              elevation: 3,
-              child: AnimatedContainer(
-                height: 10,
-                width: width * ratio,
-                duration: Duration(milliseconds: 500),
-                decoration: BoxDecoration(
-                    color: (ratio < 0.3)
-                        ? Colors.red
-                        : (ratio < 0.6)
-                            ? Colors.amber[400]
-                            : Colors.lightGreen,
-                    borderRadius: BorderRadius.circular(5)),
-              ),
-            )
-          ],
-        )
-      ],
-    );
-  }
-}
-
-class TimeState with ChangeNotifier {
-  int _time = 15;
-
-  int get time => _time;
-
-  set time(int newTime) {
-    _time = newTime;
+  set quantity(int newValue) {
+    _quantity = newValue;
     notifyListeners();
   }
 }

bbf90cd3c6ee8af2905c07b71640035db9f4ff6f
rivaldofez <rivaldofez@gmail.com>
2021-03-01 23:53:28 +0700

    using google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,9 +7,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.robotoTextTheme()
+      ),
       debugShowCheckedModeBanner: false,
-      home: Container(),
+      home: MainPage(),
     );
   }
 }
 
+class MainPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    Color fontColor = Colors.white;
+
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      appBar: AppBar(
+        backgroundColor: Colors.black45,
+        title: Text("Google Fonts Demo"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text("I'll Keep learning Flutter",
+              style: TextStyle(color: Colors.white, fontSize: 30),
+            ),
+            Text("Rivaldo Fernandes",
+              style: TextStyle(fontSize: 30, color: Colors.white
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

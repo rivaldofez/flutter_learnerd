72465a322c102adb10b73dcfeee41d2ea9bffbd2
rivaldofez <rivaldofez@gmail.com>
2021-03-02 14:55:47 +0700

    using firebase authentication with email and password

--- lib/auth_services.dart
+++ lib/auth_services.dart
@@ -15,6 +15,30 @@ class AuthServices {
    }
   }
 
+  static Future<FirebaseUser> signUp(String email, String password) async {
+    try {
+      AuthResult result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      FirebaseUser user = result.user;
+
+      return user;
+    }catch(e){
+      print(e.toString());
+      return null;
+    }
+  }
+
+  static Future<FirebaseUser> signIn(String email, String password) async {
+    try{
+      AuthResult result = await _auth.signInWithEmailAndPassword(email: email, password: password);
+      FirebaseUser user = result.user;
+
+      return user;
+    }catch(e){
+      print(e.toString());
+      return null;
+    }
+  }
+
   static Future<void> signOut() async {
     _auth.signOut();
   }
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -4,20 +4,51 @@ import 'package:flutter_learnerd/auth_services.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    TextEditingController emailController = TextEditingController(text: "");
+    TextEditingController passwordController = TextEditingController(text: "");
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Login Page"),
       ),
       body: Center(
-        child: RaisedButton(
-          onPressed: () async {
-            await AuthServices.signInAnonymous();
-          },
-          child: Text("Sign In Anonymous"),
+        child: Column(
+          children: [
+            Container(
+              width: 300,
+              height: 100,
+              child: TextField(
+                controller: emailController,
+              ),
+            ),
+            Container(
+              width: 300,
+              height: 100,
+              child: TextField(
+                controller: passwordController,
+              ),
+            ),
+            RaisedButton(
+              onPressed: () async {
+                await AuthServices.signInAnonymous();
+              },
+              child: Text("Sign In Anonymous"),
+            ),
+            RaisedButton(
+              onPressed: () async {
+                await AuthServices.signIn(emailController.text, passwordController.text);
+              },
+              child: Text("Sign In"),
+            ),
+            RaisedButton(
+              onPressed: () async {
+                await AuthServices.signUp(emailController.text, passwordController.text);
+              },
+              child: Text("Sign Up"),
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
-
